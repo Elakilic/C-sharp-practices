cc27a37b792ef982c6b1bd1fb9e05f84b89c297e
Berfin Ela Kılıç <77438033+Elakilic@users.noreply.github.com>
2023-08-22 16:10:28 +0300

    Add files via upload

---
+++ collections.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopDersleri
+{
+    class collections
+    {
+        static void main(string[] args) {
+
+            ArrayList list = new ArrayList();
+            list.Add(10);
+            list.Add("20");
+            list.Add("ali");
+            list.Add(null);
+
+            var list2 = new ArrayList() {
+                5,
+                "ela",
+                false,
+                4.5,
+                null
+            };
+            var sayi = Console.ReadLine();
+        }
+    }
+}
