225cb33cde381d0882a84d530222a8705fcabdf3
Berfin Ela Kılıç <77438033+Elakilic@users.noreply.github.com>
2023-08-23 00:55:09 +0300

    Add files via upload

---
+++ constructor.cs
@@ -0,0 +1,24 @@
+﻿class constructor
+{
+
+    
+    String name;
+    int id;
+
+    
+    constructor(String name, int id)
+    {
+        this.name = name;
+        this.id = id;
+    }
+
+    // Main Method
+    public static void Main()
+    {
+
+        
+        constructor const1 = new constructor("ela", 1);
+        Console.WriteLine("Name = " + const1.name +
+                         " and Id = " + const1.id);
+    }
+}
