e0ab33c1b5d4c13e2f0ad91a48cb478dec25689b
Berfin Ela Kılıç <77438033+Elakilic@users.noreply.github.com>
2023-08-23 11:59:05 +0300

    Add files via upload

---
+++ arrayList.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections;
+
+class arrayList
+{
+    public static void Main(string[] args)
+    {
+        ArrayList mylist = new ArrayList();
+
+        mylist.Add("A");
+        mylist.Add("B");    
+        mylist.Add("C");
+        mylist.Add("D");
+        mylist.Add("E");
+        mylist.Add("F");
+
+        Console.WriteLine("Before addRange method");
+
+        foreach(var i in mylist)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine("After addRange method");
+
+        mylist.AddRange(mylist);
+
+        foreach(var i in mylist)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
