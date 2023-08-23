d0faa897693b85414f72e670504ad571a9701ed0
Berfin Ela Kılıç <77438033+Elakilic@users.noreply.github.com>
2023-08-23 14:51:12 +0300

    Add files via upload

---
+++ nestedClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class nestedClass
+{
+    static public void Main(string[] args)
+    {
+        Dis_Class obj1 = new Dis_Class();
+        obj1.method1();
+
+        Dis_Class.ic_Class obj2 = new Dis_Class.ic_Class();
+        obj2.method2();
+
+    }
+}
+
+
+public class Dis_Class
+{
+    public void method1()
+    {
+        Console.WriteLine("Dış Class Methodu.");
+    }
+    public class ic_Class
+    {
+        public void method2()
+        {
+            Console.WriteLine("İç Class Methodu.");
+        }
+    }
+}
