63d223823422841d0ae493be8cb00fe6e1d34103
Berfin Ela Kılıç <77438033+Elakilic@users.noreply.github.com>
2023-08-23 14:39:01 +0300

    Add files via upload

---
+++ genericList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+class genericList
+{
+    static void Main(string[] args){
+
+        List<int> list = new List<int>();
+
+        list.Add(1);
+        list.Add(10);
+        list.Add(20);
+        list.Add(30);
+        list.Add(40);
+        list.Add(50);
+
+        foreach(int i in list)
+        {
+            Console.WriteLine(i);
+        }
+
+    }
+}
