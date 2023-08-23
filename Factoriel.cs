e066df06c6626e6e2910995baf37eefd3fde5128
Berfin Ela Kılıç <77438033+Elakilic@users.noreply.github.com>
2023-08-24 01:40:01 +0300

    Add files via upload

---
+++ Factoriel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Factoriel
+    {
+        static void Main()
+        {
+            for (long i = 0; i<=10;i++)
+            {
+                Console.WriteLine($"{i}!= {FactorielHesap(i)}");
+            }
+
+        }
+
+        static long FactorielHesap(long number)
+        {
+            if (number <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return number = number * FactorielHesap(number - 1);
+            }
+        }
+    }
+}
