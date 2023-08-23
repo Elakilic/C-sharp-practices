f177a7b3b45131019760c645dfdf5092974f7eec
Berfin Ela Kılıç <77438033+Elakilic@users.noreply.github.com>
2023-08-23 12:38:18 +0300

    Add files via upload

---
+++ methodsreturn.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class methodreturn
+{
+    static void Main(string[] args)
+    {
+        nesne o1 = new nesne();
+        nesne o2 = new nesne();
+
+        o1.setdata("Ela");
+        o2.setdata("Aram");
+
+        nesne o3 = o1.Astr(o2);
+
+        o1.goruntule();
+        o2.goruntule();
+        o3.goruntule();
+
+    }
+}
+
+class nesne
+{
+    private string str;
+
+    public void setdata(string s)
+    {
+        str = s;
+    }
+
+    public void goruntule() {
+        Console.WriteLine("String is: "+ str); 
+    
+    }
+    public nesne Astr(nesne n)
+    {
+        nesne obj = new nesne();
+        obj.str = str + n.str;
+        return obj;
+    }
+}
