346f6e3363e898533a1878080605fae6265daf8b
Berfin Ela Kılıç <77438033+Elakilic@users.noreply.github.com>
2023-08-25 01:03:02 +0300

    Add files via upload

---
+++ MethodOverriding.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class MethodOverriding
+    {
+        public static void Main(string[] args)
+        {
+            Hayvan hayvan = new Hayvan();
+            hayvan.Ses();
+            Kopek ciko = new Kopek();
+            ciko.Ses();
+            Kedi minik = new Kedi();
+            minik.Ses();
+
+        }
+    }
+
+    class Hayvan
+    {
+        public virtual void Ses() 
+        {
+            Console.WriteLine("----------");
+        }
+    }
+    class Kopek:Hayvan 
+    {
+        public override void Ses()
+        {
+            Console.WriteLine("Hav");
+        }
+    }
+    class Kedi : Hayvan
+    {
+        public override void Ses()
+        {
+            Console.WriteLine("Miyav");
+        }
+    }
+}
