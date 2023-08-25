c883af58ea8253d69b7fcf4fec16e662e953a029
Berfin Ela Kılıç <77438033+Elakilic@users.noreply.github.com>
2023-08-25 13:22:32 +0300

    Add files via upload

---
+++ MethodHiding.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class MethodHiding
+    {
+        public static void Main(string[] args)
+        {
+            Derived derived = new Derived();
+            derived.message();
+
+
+        }
+    }
+    class Base
+    {
+        public void message()
+        {
+            Console.WriteLine("Selam");
+
+        }
+    }
+    class Derived:Base 
+    {
+        public new void message()
+        {
+            base.message();
+            Console.WriteLine("Hello");
+
+        }
+    }
+}
